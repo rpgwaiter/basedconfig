461f7da96c0e9a29807cca5ee48c55e2e4b43037
Rpgwaiter <rpgwaiter+git@based.zone>
2021-03-02 18:53:46 -0600

    Added important hosts, started migration to open git

---
+++ hosts/nixos-gamer-laptop.nix
@@ -0,0 +1,82 @@
+{lib, config, pkgs, ... }:
+{
+   imports = with lib.lists;
+    map (x: ../.  + "/${x}") (flatten 
+    [
+      (map (x: "users/${x}")    [ "robots" "root" "borg" "storage" ])
+      (map (x: "profiles/${x}") [
+        "desktop"
+        "dev"
+        "social"
+        "networking/ssh"
+        "music"
+        "email"
+        "printing"
+        "backups/borg/gamer-laptop"
+        "hardware/yubikey"
+        "filesystems/mount-nfs"
+        "backups/syncthing"
+        "boot/systemd-boot"
+      ])
+    ]);
+
+  boot = {
+    supportedFilesystems = [ "ntfs"] ;
+    kernelPackages = pkgs.linuxPackages_latest;
+    initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+    kernelModules = [ "kvm-intel" ];
+  };
+  hardware.enableRedistributableFirmware = true;
+
+
+  environment.shellAliases = {
+    upd = "sudo nixos-rebuild switch --upgrade --flake /home/robots/git/basedconfig-ng#nixos-gamer-laptop ";
+  };
+    
+  networking = {
+    useDHCP = false;
+    firewall.enable = true;
+    networkmanager.enable = true;
+    # interfaces = {
+    #   wlo1.useDHCP = true;
+    #   enp3s0.useDHCP = true;
+    # };
+  };
+  services.xserver.layout="us";
+  
+
+  #swapDevices = [ { device = "/dev/disk/by-label"; }];
+  fileSystems = {
+    "/" = { 
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+    "/boot" = { 
+        device = "/dev/disk/by-label/boot";
+        fsType = "vfat";
+    };
+  };
+
+  users.groups.plugdev = {};
+  services.udev.extraRules = ''
+    # firmware 1.6.0+
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="2b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="3b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="4b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1807", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1808", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0000", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0001", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0004", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="1011", MODE="0660", GROUP="plugdev"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="1015", MODE="0660", GROUP="plugdev"
+    ''
+    ;
+
+  # services.basedfilehost = {
+  #   enable = true;
+  # };
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+}
