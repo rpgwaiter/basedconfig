8525b4f5c49bbe361be79501a1b99c146c22a6c0
Rpgwaiter <rpgwaiter+git@based.zone>
2021-03-05 03:04:03 +0000

    enable rtorrent config

--- profiles/media/rtorrent/default.nix
+++ profiles/media/rtorrent/default.nix
@@ -10,7 +10,6 @@
         dataDir = "/mnt/private/nix-config-store/rtorrent";
         port = 5000;
         rpcSocket = "/run/rtorrent/rpc.sock";
-        /*
         services.rtorrent.configText = lib.mkForce ''
             # Instance layout (base paths) 
             method.insert = cfg.basedir, private|const|string, (cat,"${dataDir}/")
@@ -79,6 +78,5 @@
             schedule = scgi_group,0,0,"execute.nothrow=chown,\":rtorrent\",(cfg.rpcsock)"
             schedule = scgi_permission,0,0,"execute.nothrow=chmod,\"g+w,o=\",(cfg.rpcsock)"
         '';
-        */
     };
 }
