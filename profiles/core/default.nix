63fa048f915966c4450576363b5587265de7a8c0
Rpgwaiter <rpgwaiter+git@based.zone>
2021-03-06 06:23:42 +0000

    added some old aliases back

--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -31,10 +31,44 @@ in
       whois
     ];
 
+    # shellInit = ''
+    #   export STARSHIP_CONFIG=${
+    #     pkgs.writeText "starship.toml"
+    #     (fileContents ./starship.toml)
+    #   }
+    # '';
+
     shellInit = ''
-      export STARSHIP_CONFIG=${
-        pkgs.writeText "starship.toml"
-        (fileContents ./starship.toml)
+      function extract {
+        if [ -z "$1" ]; then
+            # display usage if no parameters given
+            echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+        else
+            if [ -f $1 ] ; then
+                # NAME=''${1%.*}
+                # mkdir $NAME && cd $NAME
+                case $1 in
+                  *.tar.bz2)   tar xvjf ./$1    ;;
+                  *.tar.gz)    tar xvzf ./$1    ;;
+                  *.tar.xz)    tar xvJf ./$1    ;;
+                  *.lzma)      unlzma ./$1      ;;
+                  *.bz2)       bunzip2 ./$1     ;;
+                  *.rar)       unrar x -ad ./$1 ;;
+                  *.gz)        gunzip ./$1      ;;
+                  *.tar)       tar xvf ./$1     ;;
+                  *.tbz2)      tar xvjf ./$1    ;;
+                  *.tgz)       tar xvzf ./$1    ;;
+                  *.zip)       unzip ./$1       ;;
+                  *.Z)         uncompress ./$1  ;;
+                  *.7z)        7z x ./$1        ;;
+                  *.xz)        unxz ./$1        ;;
+                  *.exe)       cabextract ./$1  ;;
+                  *)           echo "extract: '$1' - unknown archive method" ;;
+                esac
+            else
+                echo "$1 - file does not exist"
+            fi
+        fi
       }
     '';
 
@@ -45,8 +79,16 @@ in
         # fix nixos-option
         nixos-option = "nixos-option -I nixpkgs=${toString ../../compat}";
 
-        # top
         top = "gotop";
+        myip = "dig +short myip.opendns.com @208.67.222.222 2>&1";
+        cp = "rsync -avz --progress";
+        dcu = "sudo docker-compose up -d";
+        s = "sudo systemctl";
+        mkdir = "mkdir -p";
+        conf = "cd ~/git/basedconfig-ng";
+        nas = "ssh nas";
+        dc = "sudo docker-compose";
+        pull = "git pull";
       };
   };
 
