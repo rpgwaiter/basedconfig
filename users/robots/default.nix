0710cc8837a7cd17d53cbdffa487bb9a1c8998d4
Rpgwaiter <rpgwaiter+git@based.zone>
2021-03-09 17:45:39 -0600

    passwordless sudo, very secure

--- users/robots/default.nix
+++ users/robots/default.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }:
 
 {
-  imports = [../../secrets/robots-secret.nix ];
+  imports = [ ../../secrets/robots-secret.nix ];
   users.users.robots = {
     isNormalUser = true;
     group = "robots";
     uid = 1111;
-    extraGroups = [ "storage" "wheel"  "docker" "networkmanager" "plugdev" ];
+    extraGroups = [ "storage" "wheel" "docker" "networkmanager" "plugdev" ];
   };
   users.groups.robots.gid = 1111;
+  security.sudo.extraRules = [{
+    users = [ "robots" ];
+    commands = [{ command = "ALL"; options = [ "NOPASSWD" "SETENV" ]; }];
+  }];
 }
