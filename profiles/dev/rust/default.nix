09e1014a0b1af40557a258e4752fb1c6e2dd1eb4
Rpgwaiter <rpgwaiter+git@based.zone>
2021-03-13 13:35:25 -0600

    added rust profile

---
+++ profiles/dev/rust/default.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.cargo
+    pkgs.rustc
+    pkgs.rustfmt
+  ];
+
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
